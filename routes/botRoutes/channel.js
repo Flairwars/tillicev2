b4c9641a1a6b2bca5d4051c71b5fb232fd07c15b
nate-e-97 <nathanieleeten@gmail.com>
2021-02-11 11:26:47 -0700

    Committing an example of a route endpoint, which sends a message to a given channel, since that will show structure better

---
+++ routes/botRoutes/channel.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+
+const botClient = require('../../discord/init').Client
+
+router.post('/:channelId', (req, res) => {
+    if(req.body.hasOwnProperty('msg')) {
+        botClient.channels.cache.get(req.params.channelId).send(req.body.msg)
+        res.send('Sent message')
+    }
+})
+
+module.exports = router
