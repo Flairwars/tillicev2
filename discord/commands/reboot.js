3f3e091de01c0ba750c51ffc82861e5c466888ff
thevioletstandard <76827395+thevioletstandard@users.noreply.github.com>
2021-02-13 19:49:20 -0500

    add ~reboot

---
+++ discord/commands/reboot.js
@@ -0,0 +1,9 @@
+module.exports.run = async (CommandStruct, PermStruct) => {
+    let msg = CommandStruct.message
+    await msg.reply("Shutting down. You can configure me to automatically restart elsewhere.")
+    process.exit(1)
+}
+
+module.exports.helpText = `Shuts down the bot`
+
+module.exports.Category = `Dev`
