147557ef1929d05d5911ef6f22102eeab13aa656
thevioletstandard <76827395+thevioletstandard@users.noreply.github.com>
2021-02-15 13:32:34 -0500

    add local topics

--- discord/commands/topic.js
+++ discord/commands/topic.js
@@ -6,6 +6,9 @@ module.exports.run = async (CommandStruct, PermStruct) => {
     if (CommandStruct.args.length === 0) {
         msg.reply("You need to specify a subreddit")
         return
+    } else if (CommandStruct.args[0].toLowerCase() === "tillice") {
+        let TopicText = await helpers.getNormalTopic()
+        msg.reply(embeds.SendEmbed(TopicText, `A topic from tillice`))
     } else {
         const subreddit = CommandStruct.args[0].toLowerCase()
         const TopicText = await helpers.getRandomRedditPost(subreddit)
