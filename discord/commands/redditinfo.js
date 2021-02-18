bf834f7c81c843b19193f79a48cedbba7c84cfeb
Nathaniel Eeten <blastogypsy@gmail.com>
2021-02-18 16:03:17 -0700

    redditinfo working

--- discord/commands/redditinfo.js
+++ discord/commands/redditinfo.js
@@ -1,4 +1,5 @@
 const r = require('../../reddit/init')
+const embeds = require('../template_embeds')
 
 module.exports.run = (CommandStruct, PermStruct) => {
     // TODO: First try the API to see if they're registered
@@ -6,13 +7,37 @@ module.exports.run = (CommandStruct, PermStruct) => {
         CommandStruct.message.channel.send('Please enter a reddit username after the command')
     }
     else {
-        r.getUser(CommandStruct.args[0]).fetch( thisUser => {
+        r.getUser(CommandStruct.args[0]).fetch().then( thisUser => {
             if (thisUser) {
-                console.log(thisUser)
+                let dateCreated = new Date(thisUser.created_utc*1000)
+                let totalKarma = thisUser.total_karma
+                let commentKarma = thisUser.comment_karma
+                let linkKarma = thisUser.link_karma
+                let awarderKarma = thisUser.awarder_karma
+                let awardeeKarma = thisUser.awardee_karma
+                let username = `/u/${thisUser.name}`
+
+                r.getSubreddit('flairwars').getUserFlair(thisUser.name).then( userFlair => {s
+
+                    CommandStruct.message.channel.send(
+                        embeds.redditInfoEmbed(
+                            userFlair.flair_css_class.toLowerCase(),
+                            username,
+                            dateCreated.toDateString(),
+                            totalKarma,
+                            linkKarma,
+                            commentKarma,
+                            awarderKarma,
+                            awardeeKarma
+                        )
+                    )
+                })
             }
             else {
                 CommandStruct.message.channel.send(`I couldn't find the user ${CommandStruct.args[0]}, try again`)
             }
+        }).catch( err => {
+            CommandStruct.message.channel.send(`I couldn't find the user ${CommandStruct.args[0]}, try again`)
         })
     }
 }
--- discord/template_embeds.js
+++ discord/template_embeds.js
@@ -3,17 +3,56 @@ const Discord = require('discord.js')
 let BaseEmbed = () => {
     return new Discord.MessageEmbed()
         .setColor('#888888')
-        .setFooter('Tilice v2')
+        .setFooter('Tillice v2')
 }
 
 
 let ErrorEmbed = () => {
     return new Discord.MessageEmbed()
         .setColor('#B10c06')
-        .setFooter('Tilice v2')
+        .setFooter('Tillice v2')
         .setImage('https://i.imgur.com/5b3Misq.png')
 }
 
+let fwFlavoredEmbed = (color) => {
+    let newEmbed = new Discord.MessageEmbed()
+        switch(color) {
+            case "red":
+                newEmbed.setColor('#ff0000')
+                newEmbed.setThumbnail('https://i.imgur.com/Mi8J0fq.png')
+                break;
+            case "orange":
+                newEmbed.setColor('#ff8800')
+                newEmbed.setThumbnail('https://media.discordapp.net/attachments/703431121857282108/711885744280436736/3_5.png')
+                break;
+            case "yellow":
+                newEmbed.setColor('#ffff00')
+                newEmbed.setThumbnail('https://i.imgur.com/WB8mUHl.png')
+                break;
+            case "green":
+                newEmbed.setColor('#00ff00')
+                newEmbed.setThumbnail('https://i.imgur.com/MNKwjES.jpg')
+                break;
+            case "blue":
+                newEmbed.setColor('#0000ff')
+                newEmbed.setThumbnail('https://i.imgur.com/IymoMsy.png')
+                break;
+            case "purple":
+                newEmbed.setColor('#880088')
+                newEmbed.setThumbnail('https://i.imgur.com/rZFSCIP.jpg')
+                break;
+            case "mod":
+                newEmbed.setColor('#ffffff')
+                newEmbed.setThumbnail('https://cdn.discordapp.com/attachments/506018154078666752/590846311775862785/yeghet.png')
+                break;
+            default:
+                newEmbed.setColor('#888888')
+                break;
+        }       
+        newEmbed.setFooter('Tillice v2')
+
+        return newEmbed
+}
 
 module.exports.GeneralHelpEmbed = (HelpResponseStruct) => {
     let Embed = BaseEmbed()
@@ -96,5 +135,16 @@ module.exports.suggestionDiscussionEmbed = (user, userPfp, suggestion, link) => 
     .setDescription(suggestion)
     .setTimestamp()
     .addField('Talk about it!', `[Vote on it](${link})`)
+    return Embed
+}
+
+module.exports.redditInfoEmbed = (color, username, creationDate, totalKarma, linkKarma, commentKarma, awarderKarma, awardeeKarma) => {
+    let Embed = fwFlavoredEmbed(color)
+    Embed.setTitle(username)
+    Embed.setDescription(`[${username}](https://reddit.com${username})`)
+    Embed.addField('Flair', color, true)
+    Embed.addField('Account created', creationDate, true)
+    Embed.addField('Total Karma', totalKarma, true)
+    Embed.addField('Karma Breakdown', `Post karma: ${linkKarma}\nComment Karma: ${commentKarma}\nAwarder Karma: ${awarderKarma}\nAwardee Karma: ${awardeeKarma}`)
     return Embed
 }
