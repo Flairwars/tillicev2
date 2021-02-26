b640bb59cba10da8042a4b04a449aeff969453da
nate-e-97 <nathanieleeten@gmail.com>
2021-02-26 13:59:50 -0700

    pollsuggestions is finished

--- discord/commands/pollsuggestions.js
+++ discord/commands/pollsuggestions.js
@@ -3,6 +3,8 @@ const perms = require('../eval_perms')
 const guildCfg = require('../guildCfg')
 const client = require('../init')
 
+const Discord = require(`discord.js`);
+
 module.exports.run = async (CommandStruct, PermStruct) => {
     
     let suggestionsChannel = CommandStruct.message.guild.channels.cache.get(guildCfg.suggestions)
@@ -10,10 +12,11 @@ module.exports.run = async (CommandStruct, PermStruct) => {
 
     let suggestionMessages = await suggestionsChannel.messages.fetch({limit: 100})
 
-    let suggestions = suggestionMessages.filter(msg => msg.author === client.Client.user)
-                                        .filter(msg => msg.embeds.length > 0)
-                                        .filter(msg => msg.embeds[0].author.name.startsWith('Suggestion by '))
-                                        .filter(msg => !msg.embeds[0].footer.text.startsWith('Suggestion was '))
+    let suggestions = suggestionMessages.filter(msg => msg.author === client.Client.user) // Filter messages not made by the bot
+                                        .filter(msg => msg.embeds.length > 0) // Filter messages with no embeds
+                                        .filter(msg => msg.embeds[0].author.name.startsWith('Suggestion by ')) // Only suggestions please
+                                        .filter(msg => !msg.embeds[0].footer.text.startsWith('Suggestion was ')) // No suggestions that were already accepted/denied
+                                        .filter(msg => !msg.embeds[0].footer.text.startsWith('Suggestion is being ')) // No suggestions already being polled
                                         .array()
     
     if (CommandStruct.args.length > 0) {
@@ -26,12 +29,39 @@ module.exports.run = async (CommandStruct, PermStruct) => {
     suggestions.forEach(suggestion => {
         let suggestionEmbed = suggestion.embeds[0]
 
+        let acceptedSuggestions = 0
+
+        let embed = new Discord.MessageEmbed(suggestionEmbed)
+
         const likes = suggestion.reactions.cache.get("👍").count-1
         const dislikes = suggestion.reactions.cache.get("👎").count-1
 
-        console.log(`Suggestion ID ${suggestion.id}: ${likes} likes and ${dislikes} dislikes`)
+        if (likes < 1) { // Actual threshold is 15
+            // Suggestion denied
 
-        
+            embed.setAuthor(suggestionEmbed.author.name, suggestionEmbed.author.iconURL)
+                .setColor('#FF0000')
+                .setFooter(`Suggestion was rejected`);
+
+        } else {
+            // Send poll to modpollchannel
+            acceptedSuggestions += 1;
+
+            embed.setAuthor(`${acceptedSuggestions}: ${suggestionEmbed.author.name}`)
+                 .setColor('#FFFF00')
+                 .setFooter(`${likes} 👍, ${dislikes} 👎 | ID: ${suggestion.id}`)
+            
+            modPollChannel.send(embed).then( msg => {
+                msg.react("👍");
+                msg.react("👎");
+                msg.react("🤷");
+            })
+
+            embed.setAuthor(suggestionEmbed.author.name, suggestionEmbed.author.iconURL)
+                 .setFooter(`Suggestion is being polled in mod channels.`);
+        }
+
+        suggestion.edit('', {embed: embed})
     })
 
 }
