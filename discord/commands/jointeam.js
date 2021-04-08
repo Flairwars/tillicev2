edffb970df66213b75bc7ad04bd0e0110ab8fc63
jarwho11 <40441179+jarwho11@users.noreply.github.com>
2021-04-08 21:09:30 +0100

    ~jointeam Update for B33.5
    - Updated ~jointeam command for Battle 33.5 (albeit rushed, so it might be broken in a way that I haven't discovered yet lol)

--- discord/commands/jointeam.js
+++ discord/commands/jointeam.js
@@ -3,13 +3,19 @@ const Discord = require("discord.js");
 
 // The run function should ALWAYS take CommandStruct and PermStruct
 module.exports.run = (CommandStruct, PermStruct) => {
-  const teamRoles = ["Single Slash", "Double Slash", "No Slash"];
+
+  const teamRoles = ["single slash", "double slash", "no slash"];
   const teamPrefixes = ["u/", "/u/", ""];
   const client = require('../init').Client;
-  let guild = client.guilds.cache.get('463794005231271976');
+  let guild = client.guilds.cache.get('809491629207191572'); // MEGASERVER : 463794005231271976 // BOT-TESTING GUILD : 809491629207191572
   let member = guild.member(CommandStruct.message.author);
   let userNick = member ? member.displayName : null;
-  let finalNick = userNick.substring(3);
+
+  teamChosen = CommandStruct.args.join(" ");
+  let invalidEmbed = new Discord.MessageEmbed()
+  .setDescription("**invalid selection**")
+  .setColor("#f21313")
+  if (!teamRoles.includes(teamChosen.toLowerCase())) return CommandStruct.message.channel.send(invalidEmbed);
 
   const roleCheck = CommandStruct.message.member.roles.cache.find(role => teamRoles.includes(role.name));
   let declineEmbed = new Discord.MessageEmbed()
@@ -17,19 +23,28 @@ module.exports.run = (CommandStruct, PermStruct) => {
   .setColor("#f21313")
   if(roleCheck) return CommandStruct.message.channel.send(declineEmbed);
 
-  let choice = (Math.round(Math.random() * (3 - 1) + 1));
+  let choice = 1;
+
+  if(teamChosen.toLowerCase() == "single slash") {
+    choice = 0;
+  } else if (teamChosen.toLowerCase() == "double slash") {
+    choice = 1;
+  } else if (teamChosen.toLowerCase() == "no slash") {
+    choice = 2;
+  };
+
   let acceptEmbed = new Discord.MessageEmbed()
   .setDescription("**You have been assigned to:**")
-  .addField(teamRoles[choice-1].toUpperCase(), "This action is irreversable")
+  .addField(teamRoles[choice].toUpperCase(), "This action is irreversable")
   .setColor("#0cca00")
   CommandStruct.message.channel.send(acceptEmbed);
   let assignedRole = CommandStruct.message.guild.roles.cache.find(role => role.name === teamRoles[choice-1]);
   if(assignedRole) CommandStruct.message.member.roles.add(assignedRole.id);
-  CommandStruct.message.member.setNickname(teamPrefixes[choice-1] + finalNick);
+  CommandStruct.message.member.setNickname(teamPrefixes[choice] + userNick);
 }
 
 // This should be a string, it will be used in the detailed help command for a specific command
-module.exports.helpText = `Used to randomly assign a user to a battle 33 team`
+module.exports.helpText = `Allows the user to assign themself to a battle 33.5 team`
 
 // This should be a string. It will be used for general help to list commands by category
 module.exports.Category = `Misc`
